1f331d0b9ff412d8459518ebdac5d8a28c405734
WILLIAM-ERIK <86015215+WILLIAM-ERIK@users.noreply.github.com>
2022-10-12 01:01:12 -0400

    Add files via upload

---
+++ PREG1/matematica/index.php
@@ -0,0 +1,44 @@
+
+<!-- Cabecera -->
+<?php 
+    include "../header.inc.php";
+?>
+
+<!-- Cuerpo -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="../index.php">FCPN</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" aria-current="page" href="../informatica/index.php">INFORMÁTICA</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="#!">MATEMÁTICA</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../fisica/index.php">FÍSICA</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <header class="py-5 bg-image-full" style="background-image: url('../imagenes/06 FONDO MATE.jpg')">
+            <div class="text-center my-5">
+                <img class="img-fluid rounded-circle mb-4" src="../imagenes/05 LOGO MATE.JPG" alt="..." />
+                <h1 class="text-white fs-1 fw-bolder">CARRERA DE MATEMÁTICA</h1>
+                <p class="text-white-50 mb-1 fs-1">UNIVERSIDAD MAYOR DE SAN ANDRES</p>
+            </div>
+        </header>
+
+        <section class="py-5">
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h2 style="color:#BA4A00">Carrera de Matemática</h2>
+                        <p class="lead">La Carrera de Matemática es una Unidad Académica dependiente de la Facultad de Ciencias Puras y Naturales (FCPN) de la Universidad Mayor de San Andrés (UMSA), cuya misión principal es la formación profesional en Matemática a nivel superior y desarrollar la investigación matemática dentro del Instituto de Investigación Matemática (IIMAT), donde se ejecutan proyectos en las diversas áreas de la matemática como Algebra, Análisis, Geometría, Topología, Optimización Matemática, Modelos Matemáticos, Análisis Numérico, Análisis Cuantitativo de Datos y en otras áreas de aplicaciones matemáticas como proyectos multidisciplinarios o transdisciplinarios.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<!-- Footer-->
+<?php 
+    include "../footer.inc.php";
+?>
